fd42d917f41e77c3aab0e393ca41b87325fd4d01
nakanokurenai <master@yukarium.net>
2018-09-25 16:48:19 +0900

    Use 128MB runtime on auth functions

--- functions/src/auth/index.ts
+++ functions/src/auth/index.ts
@@ -3,7 +3,12 @@ import * as Functions from 'firebase-functions'
 import config from '../config'
 import { firestore } from '../preload'
 
-export const onCreate = Functions.auth.user().onCreate(async (user) => {
+const runtimeOpts = {
+  timeoutSeconds: 30,
+  memory: '128MB' as '128MB'
+}
+
+export const onCreate = Functions.runWith(runtimeOpts).auth.user().onCreate(async (user) => {
   const ref = firestore.collection('accounts').doc()
   await ref.set({
     attributes: [
@@ -15,7 +20,7 @@ export const onCreate = Functions.auth.user().onCreate(async (user) => {
   })
 })
 
-export const onDelete = Functions.auth.user().onDelete(async (user) => {
+export const onDelete = Functions.runWith(runtimeOpts).auth.user().onDelete(async (user) => {
   const qs = await firestore.collection('accounts').where("uid", "==", user.uid).get()
   const p = []
   qs.forEach(

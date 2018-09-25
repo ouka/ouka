6c87d386200079fdc1e1cde3dde9d9fba996859d
nakanokurenai <master@yukarium.net>
2018-09-25 16:57:35 +0900

    use new timestamp behavior of Firestore

--- functions/src/preload.ts
+++ functions/src/preload.ts
@@ -3,3 +3,6 @@ import { initializeApp, firestore as Firestore } from 'firebase-admin'
 initializeApp()
 
 export const firestore = Firestore()
+firestore.settings({
+  timestampsInSnapshots: true
+})

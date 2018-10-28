44bc0cae37ed148862fbba76a32adaac62809f81
nakanokurenai <master@yukarium.net>
2018-10-29 01:28:22 +0900

    *WIP*

--- functions/src/functions/firestore/index.ts
+++ functions/src/functions/firestore/index.ts
@@ -2,6 +2,8 @@ import * as Functions from "firebase-functions"
 import Joi = require('joi')
 import Actor from "~/models/actor";
 
+import * as types from '@ouka/activity-vocabulary/types'
+
 const runtimeOpts = {
   timeoutSeconds: 30,
   memory: '128MB' as '128MB'
@@ -11,35 +13,68 @@ const runtimeOpts = {
  * TODO: Add error reporting!
  */
 
-export const outbox = Functions.runWith(runtimeOpts).firestore.document('accounts/{id}/outbox/{param}').onCreate(async (doc, ctx) => {
-  const validateTarget = Joi.alternatives().try(Joi.string())
+// FIXME: move this to lib/actibitypub OR lib/activity-vocabulary
+type Activity = {
+  '@context': string | string[], // JSON-LD
+  type: string,
+  to: string | string[],
+  bto?: string | string[],
+  cc?: string | string[],
+  bcc?: string | string[],
+  audience?: string,
+  object?: any // we can validate later
+}
 
-  const { error, value: activity } = Joi.object().required().keys({
-    type: Joi.any().required().allow('Follow', 'Accept', 'Create', 'Note'),
+export const outbox = Functions.runWith(runtimeOpts).firestore.document('accounts/{id}/outbox/{param}').onCreate(async (doc, ctx) => {
+  const actor = await Actor.findById(ctx.params.id)
+
+  const validateTarget = Joi.alternatives().try(Joi.string(), Joi.array().items(Joi.string()))
+  const { error, value } = Joi.object().required().keys({
+    type: Joi.any().required().allow(...types.Activity, ...types.Object),
     to: validateTarget.required(),
     bto: validateTarget,
     cc: validateTarget,
     bcc: validateTarget,
     audience: validateTarget,
-  }).unknown(true).validate<{
-    type: string,
-    to: string,
-    bto?: string,
-    cc?: string,
-    bcc?: string,
-    audience?: string
-  }>(doc.data() as any)
+  }).unknown(true).validate<Activity>(doc.data() as any)
   if (error) throw error
 
-  const actor = await Actor.findById(ctx.params.id)
+  // 除外する || フラグにする: public collection
+  const receivers = [
+    ...(Array.isArray(value.to) ? value.to : [value.to]),
+    ...(Array.isArray(value.bto) ? value.bto : (typeof value.bto === 'undefined' ? [value.bto] : [])),
+    ...(Array.isArray(value.cc) ? value.cc : (typeof value.cc === 'undefined' ? [value.cc] : [])),
+    ...(Array.isArray(value.bcc) ? value.bcc : (typeof value.bcc === 'undefined' ? [value.bcc] : [])),
+  ]
 
-  const targets = await Promise.all([
-    activity.to,
-    ...(activity.bto ? [activity.bto] : []),
-    ...(activity.cc ? [activity.cc] : []),
-    ...(activity.bcc ? [activity.bcc] : []),
-    ...(activity.audience ? [activity.audience] : []),
-  ].map(target => Actor.fetch(target)))
+  const activity = (() => {
+    if (!types.Object.includes(value.type)) return {...value}
+
+    const a = {
+      '@context': ['https://www.w3.org/ns/activitystreams'],
+      to: value.to,
+      object: {
+        ...value
+      }
+    } as Activity
+    delete a.object['@context']
+    if (typeof value.bto === 'undefined') a.bto = value.bto
+    if (typeof value.cc === 'undefined') a.cc = value.cc
+    if (typeof value.bcc === 'undefined') a.bcc = value.bto
+
+    return a
+  })() 
+
+  // remove blind receivers
+  delete activity.bto
+  delete activity.bcc
+  if ('object' in activity) {
+    delete activity.object.bto
+    delete activity.object.bcc
+  }
+
+  // Too Wip (not support collection...)
+  const targets = await Promise.all(receivers.map(v => Actor.fetch(v)))
 
   return Promise.all(targets.map(v => actor.send(v, activity)))
 })
